5f89e56660c07d16a21859fb427c685fa34d89fb
Mohammad Hajeer <mohammadhajeer002@gmail.com>
2022-10-21 20:59:54 +0300

    Modified The Template + Update New Things

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
 // Calling The Elements
+// Header
 let header = document.querySelector("header");
 let menuBtn = document.querySelector(".nav-bars > i");
 let navBarMenu = document.querySelector(".nav-bars ul");
 let links = document.querySelectorAll(".nav-bars ul li a");
+
+// Scroll Button
 let scrollToTopButton = document.querySelector("header .scroll-to-top");
+
+// Portfolio Section
 let portfolioNavBar = document.querySelectorAll(".portfolio .categories span");
 let portfolioContent = document.querySelectorAll(".portfolio .content > div");
-console.log(portfolioContent);
+
+// Statistics Section
+let statisticsSection = document.querySelector(".statistics");
+let statisticsNumbers = document.querySelectorAll(
+  ".statistics .container .status p:first-of-type"
+);
+
+// Testimonial Section
+let testimonialSkillSection = document.querySelector(".testimonials-skills");
+let progressBars = document.querySelectorAll(
+  ".testimonials-skills .skills .progress"
+);
+
+// Landing Section
+let landingSection = document.querySelector(".landing");
+let rightArrow = document.querySelector(".landing .right-btn");
+let leftArrow = document.querySelector(".landing .left-btn");
+let landingBullets = Array.from(
+  document.querySelectorAll(".landing .bullets span")
+);
 
 // Open-Close Menu
 menuBtn.onclick = () => {
   navBarMenu.classList.toggle("toggle");
 };
 
+let startedCounting = false;
+let startedProgressBar = false; // start the function
 // Scrolling To Top
 window.onscroll = () => {
   if (scrollY > 500) {
@@ -29,6 +55,23 @@ window.onscroll = () => {
     scrollToTopButton.style.display = "none";
     header.style.background = "none";
     header.style.position = "fixed";
+  }
+
+  if (scrollY >= testimonialSkillSection.offsetTop - 150) {
+    if (!startedProgressBar) {
+      progressBars.forEach((progress) => {
+        progressBar(progress, 1500);
+        progress.style.width = progress.dataset.number;
+      });
+    }
+    startedProgressBar = true;
+  }
+
+  if (scrollY >= statisticsSection.offsetTop - 300) {
+    if (!startedCounting) {
+      statisticsNumbers.forEach((number) => progressBar(number, 1));
+    }
+    startedCounting = true;
   }
 };
 
@@ -69,3 +112,57 @@ portfolioNavBar.forEach((category) => {
     });
   });
 });
+
+function progressBar(progress, seconds) {
+  let goal = +progress.dataset.number.match(/\d+/g);
+  let incerment = setInterval(() => {
+    progress.dataset.text++;
+    if (+progress.dataset.text == goal) {
+      clearInterval(incerment);
+    }
+  }, seconds / goal);
+}
+
+// Changing Landing Section WallPaper
+let landingWallPapers = [
+  "url(images/landing-image02.jpg)",
+  "url(images/landing-image01.jpg)",
+  "url(images/landing-image03.webp)",
+];
+
+let currentLandingPaper = 1;
+
+function displayWallPaper(index) {
+  landingSection.style.backgroundImage = landingWallPapers[index];
+}
+
+function removeAddActiveClass(index) {
+  landingBullets.forEach((bullet) => {
+    bullet.classList.remove("active");
+  });
+  landingBullets[index].classList.add("active");
+}
+
+rightArrow.onclick = () => {
+  if (
+    currentLandingPaper >= 0 &&
+    currentLandingPaper < landingWallPapers.length - 1
+  ) {
+    currentLandingPaper++;
+  }
+  console.log(currentLandingPaper);
+  displayWallPaper(currentLandingPaper);
+  removeAddActiveClass(currentLandingPaper);
+};
+
+leftArrow.onclick = () => {
+  if (
+    currentLandingPaper > 0 &&
+    currentLandingPaper <= landingWallPapers.length - 1
+  ) {
+    currentLandingPaper--;
+  }
+
+  displayWallPaper(currentLandingPaper);
+  removeAddActiveClass(currentLandingPaper);
+};
